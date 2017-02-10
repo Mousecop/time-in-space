7b55f99f64187975384e90d947e60834303b5497
Dave Hoel <dkhoel@gmail.com>
2017-02-10 13:30:22 -0700

    commite b4 merge

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-var appState = {
+var appState = { // Application state object
     NASA_BASE_URL: 'https://api.nasa.gov/planetary/apod',
     dataArr: [{
+        // Starting object pulled from the API from 02/09/2017
+        // To have an image for initial render.
         "date": "2017-02-09",
         "explanation": "Peering from the shadows, the Saturn-facing hemisphere of tantalizing inner moon Enceladus poses in this Cassini spacecraft image. North is up in the dramatic scene captured last November as Cassini's camera was pointed in a nearly sunward direction about 130,000 kilometers from the moon's bright crescent. In fact, the distant world reflects over 90 percent of the sunlight it receives, giving its surface about the same reflectivity as fresh snow. A mere 500 kilometers in diameter, Enceladus is a surprisingly active moon. Data collected during Cassini's flybys and years of images have revealed the presence of remarkable south polar geysers and a possible global ocean of liquid water beneath an icy crust.",
         "hdurl": "http://apod.nasa.gov/apod/image/1702/PIA20522enceladus.jpg",
@@ -9,12 +11,14 @@ var appState = {
         "title": "Crescent Enceladus",
         "url": "http://apod.nasa.gov/apod/image/1702/PIA20522enceladusC.jpg"
     }],
+    // Keys for page elements to manage state.
     datePicker: true,
     submitButton: true,
     infoButton: false,
     newDate: false
 };
-
+// Submit query to API - called from $(function eventHandler()
+// at the submit handler.
 function getNasaData(searchTerm, callback) {
     var query = {
         api_key: 'V5am4SFkNFMjACvm9UjIvNOQSOIIfktcKIeoIUp5',
@@ -22,40 +26,44 @@ function getNasaData(searchTerm, callback) {
     };
     $.getJSON(appState.NASA_BASE_URL, query, callback);
 }
-
+// getJSON Callback function.
 function callbackFunc(data) {
     appState.dataArr[0] = data;
-    console.log(data);
     renderState(appState, $('html'));
 }
-
+// Compare user selcted date to alert if a future date or a date prior to
+// earliest archive image was selected.
 function dateCompare(date) {
   var selectedDate = date.slice(0,4) + date.slice(5,7) + date.slice(8);
   var today = new Date();
-  var currentDate = today.getFullYear().toString() + '0' +
-   (today.getMonth()+1).toString() + today.getDate().toString();
+  // getMonth and getDate return an int - need to pad a '0' for single digit
+  // months and days.
+  var currentMonth = ('0' + (today.getMonth()+1).toString()).slice(-2);
+  var currentDay = ( '0' + today.getDate().toString()).slice(-2);
+  var currentDate = today.getFullYear().toString() + currentMonth + currentDay;
   if (selectedDate > currentDate) {
-    return 0;
+    return 0; // Return 0 triggers a future date alert
   } else if (selectedDate < 19950617) {
-    return 1;
+    return 1; // Return 1 triggers a date prior to archive start date alert
   } else {
-    return 2;
+    return 2; // Return 2 allows normal processing
   }
 }
-
+// Modify application state keys - true / false - called from event handlers.
 function modKeyState(key) {
     appState[key] = !appState[key];
 }
-
+// Check the true/false state of application state keys to show or hide elements
+// Called from renderState function.
 function checkState(key) {
     appState[key] ? $('.' + key).show() : $('.' + key).hide();
 }
-
+// Adds photo description information to the page
 function moreInfo(state, element) {
     var infoHtml = '<p class="information">' + state.dataArr[0].explanation + '</p>';
     element.append(infoHtml);
 }
-
+// Renders the DOM depending on application state
 function renderState(state, element) {
     element.css('background', 'url("' + state.dataArr[0].url + '")' + 'no-repeat center center fixed');
     element.css('background-size', 'cover');
@@ -64,39 +72,42 @@ function renderState(state, element) {
     checkState('submitButton');
     checkState('newDate');
 }
-
+// Event handlers
 $(function eventHandler() {
     renderState(appState, $('html'));
+    // Submit button handler
     $('form').submit(function(e) {
         e.preventDefault();
         var date = $('#date-input').val();
         var comparedDates = dateCompare(date);
         if (comparedDates === 0) {
-          alert('Date selected is in the future');
+          alert('You cannot select a date in the future');
         } else if (comparedDates === 1) {
           alert('There are no APODs before 6/17/1995')
         } else {
           getNasaData(date, callbackFunc);
-          $('.title').hide();
+          $('.title').hide(); // Hide page title
+          // Change the true/false state of app state keys
           modKeyState('infoButton');
           modKeyState('submitButton');
           modKeyState('datePicker');
         }
-
     });
-
+    // Show Info button handler
     $('.infoButton').click(function(event) {
-        moreInfo(appState, $('.info'));
+        moreInfo(appState, $('.info')); // Call funtion to display photo description
+        // Change the true/false state of app state keys
         modKeyState('infoButton');
         modKeyState('newDate');
         renderState(appState, $('html'));
     });
-
+    // New Date button handler
     $('.newDate').click(function(event) {
+        // Change the true/false state of app state keys
         modKeyState('newDate');
         modKeyState('datePicker');
         modKeyState('submitButton');
         renderState(appState, $('html'));
-        $('p').remove('.information');
+        $('p').remove('.information'); // Remove photo description
     });
 });
